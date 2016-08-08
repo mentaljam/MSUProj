f2405d2f7a3814ca747d7b5f55a33a63a753b2a2
Petr Tsymbarovich <petr@tsymbarovich.ru>
2016-08-08 10:21:49 +0300

    Update translations
    - Update .tx/config
    - Add translations for the installer scripts

---
+++ package/qtifw/component-msuproj_ru_RU.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>component-msuproj</name>
+    <message>
+        <source>Setup MSUProj</source>
+        <comment>Start menu entry</comment>
+        <translation>Настройка MSUProj</translation>
+    </message>
+    <message>
+        <source>Project Page</source>
+        <comment>Start menu link</comment>
+        <translation>Страница проекта</translation>
+    </message>
+    <message>
+        <source>Issues and Suggestions</source>
+        <comment>Start menu link</comment>
+        <translation>Вопросы и предложения</translation>
+    </message>
+    <message>
+        <source>Robonuka Meteor-M N2 Site</source>
+        <comment>Start menu link</comment>
+        <translation>Сайт Robonuka Meteor-M N2</translation>
+    </message>
+</context>
+</TS>
