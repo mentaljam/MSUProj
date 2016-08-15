26857d9b71068df883a38360a3691c32c0906fb1
Petr Tsymbarovich <petr@tsymbarovich.ru>
2016-08-15 17:22:53 +0300

    Update translations

---
+++ package/qtifw/component-msuproj_hu_HU.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>component-msuproj</name>
+    <message>
+        <location filename="component-msuproj.qs" line="13"/>
+        <source>Setup MSUProj</source>
+        <comment>Start menu entry</comment>
+        <translation>MSUProj telepítése</translation>
+    </message>
+    <message>
+        <location filename="component-msuproj.qs" line="15"/>
+        <source>Project Page</source>
+        <comment>Start menu link</comment>
+        <translation>Projekt honlapja</translation>
+    </message>
+    <message>
+        <location filename="component-msuproj.qs" line="18"/>
+        <source>Issues and Suggestions</source>
+        <comment>Start menu link</comment>
+        <translation>Problémák és Javaslatok</translation>
+    </message>
+    <message>
+        <location filename="component-msuproj.qs" line="21"/>
+        <source>Robonuka Meteor-M N2 Site</source>
+        <comment>Start menu link</comment>
+        <translation>Robonuka Meteor-M N2 oldala</translation>
+    </message>
+</context>
+</TS>
